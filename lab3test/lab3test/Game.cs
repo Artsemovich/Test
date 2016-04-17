3c5406983d26ef0be3ba346afe558a5ad2b8af83
Artsemovich <yulya.artsemovich@mail.ru>
2016-04-17 22:42:54 +0600

    класс игры с частичной реализацией

--- lab3test/lab3test/Game.cs
+++ lab3test/lab3test/Game.cs
@@ -120,7 +120,178 @@ namespace lab3test
             }
         }
     }
+    public class PatternsGame
+    {
+        public List<Question> Questions; // Список вопросов игры
+        public int CorrectAnswers; // Количество правильных ответов, выбранных игроком
+        public int IncorrectAnswers; // Количество неправильных ответов, выбранных игроком
+        public int Counter; // Номер текущего вопроса
+        public bool On; // Флаг, что идёт игра
 
+        // Статический метод, возвращает название паттерна
+        public static String PatternString(PatternName pattern)
+        {
+            switch (pattern)
+            {
+                case PatternName.Delegation:
+                    return "Делегирование";
+                case PatternName.DelegationEventModel:
+                    return "Модель на делегатах";
+                case PatternName.Proxy:
+                    return "Прокси";
+                case PatternName.Adapter:
+                    return "Адаптер";
+                case PatternName.Decorator:
+                    return "Декторатор";
+                    break;
+                case PatternName.Bridge:
+                    return "Мост";
+                case PatternName.Composite:
+                    return "Компоновщик";
+                case PatternName.Flyweight:
+                    return "Приспоспобленец";
+                case PatternName.Iterator:
+                    return "Итератор";
+                case PatternName.InformationExpert:
+                    return "Информационный эксперт";
+                case PatternName.Factory:
+                    return "Фабрика";
+                case PatternName.FactoryMethod:
+                    return "Фабричный метод";
+                case PatternName.AbstractFactory:
+                    return "Абстрактная фабрика";
+                case PatternName.Prototype:
+                    return "Прототип";
+                case PatternName.Builder:
+                    return "Строитель";
+                case PatternName.Singleton:
+                    return "Одиночка";
+                case PatternName.ObjectPool:
+                    return "Пул объектов";
+                case PatternName.State:
+                    return "Состояние";
+                case PatternName.Observer:
+                    return "Наблюдатель";
+                case PatternName.Memento:
+                    return "Хранитель";
+                case PatternName.Command:
+                    return "Команда";
+                case PatternName.Visitor:
+                    return "Посетитель";
+                case PatternName.Indirection:
+                    return "Перенаправление";
+                case PatternName.ChainOfResponsibility:
+                    return "Цепочка обязанностей";
+                case PatternName.Mediator:
+                    return "Посредник";
+                case PatternName.Strategy:
+                    return "Стратегия";
+                case PatternName.Interpreter:
+                    return "Интерпретатор";
+                case PatternName.TemplateMethod:
+                    return "Шаблонный метод";
+                case PatternName.Controller:
+                    return "Контроллер";
+                case PatternName.PureFabrication:
+                    return "Искусственный";
+                case PatternName.DontTalkToStrangers:
+                    return "Не разговаривайте с неизвестными";
+                case PatternName.MVC:
+                    return "MVC";
+                case PatternName.NoPattern:
+                    return "Паттернов нет";
+            }
+            return "";
+        }
+
+        // Конструктор по умолчанию
+        public PatternsGame()
+        {
+            ;
+        }
+
+        // Добавляет вопрос к списку вопросов игры
+        public void AddQuestion(Question q)
+        {
+            ;
+        }
+
+        // Создает вопрос типа "угадать паттерн по коду" и добавляет его к списку вопросов
+        public void AddQuestion(PatternName answer, String code)
+        {
+            ;
+        }
+
+        // Создает вопрос типа "угадать паттерн по изображению" и добавляет его к списку вопросов
+        public void AddQuestion(PatternName answer, Image picture)
+        {
+            ;
+        }
+
+        // Если идёт игра, возвращает текущий вопрос. Иначе возвращает null
+        public Question CurrentQuestion()
+        {
+            return null;
+        }
+
+        // Если выбранный пользователем ответ правильный, увеличивает счётчик правильных ответов, иначе увеличивает счетчик неправильных.
+        // После чего увеличивает счётчик текущего вопроса.
+        // Если вопросы закончились, завершает игру.
+        public void SetAnswer(PatternName Answer)
+        {
+            ;
+        }
+
+        
+
+        // Возвращает количество вопросов в игре
+        public int GetQuestionsCount()
+        {
+            return Questions.Count;
+        }
+
+        // Возвращает количество выбранных игроком правильных ответов
+        public int GetCorrectAnswers()
+        {
+            return CorrectAnswers;
+        }
+
+        // Возвращает количество выбранных игроком неправильных ответов
+        public int GetIncorrectAnswers()
+        {
+            return IncorrectAnswers;
+        }
+
+        // Возвращает счетчик текущего вопроса
+        public int GetCounter()
+        {
+            return Counter;
+        }
+
+        // Обнуляет параметры игры, но не запускает игру
+        public void NewGame()
+        {
+           ;
+        }
+
+        // Проверяет, идет ли игра
+        public bool IsOn()
+        {
+            return On;
+        }
+
+        // Запускает игру, но не обнуляет её параметры
+        public void Start()
+        {
+            On = true;
+        }
+
+        // Завершает игру
+        public void Stop()
+        {
+            On = false;
+        }
+    };
 
 }
 
